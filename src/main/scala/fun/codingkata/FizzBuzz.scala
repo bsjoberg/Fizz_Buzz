3ff8ebc97b6e8179e3237a09b4cc065a77c244ed
bsjoberg <brian.sjoberg@excella.com>
2019-11-19 08:14:01 -0500

    Initial unit test for getresult that does the minimal to make the test pass. Changed the scoverage version to latest stable release.

--- src/main/scala/fun/codingkata/FizzBuzz.scala
+++ src/main/scala/fun/codingkata/FizzBuzz.scala
@@ -1,5 +1,7 @@
 package fun.codingkata
 
 object FizzBuzz {
+  def getResult(number: Int): String = "fizz"
+
   def countTo(count: Int): String = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz"
 }
